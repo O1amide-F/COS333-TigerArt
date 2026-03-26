f70f94020d015b80b98e689c7c9ca8aad3ff3fc8
Olamide Falayi <of5697@princeton.edu>
2026-03-26 17:24:01 -0400

    Updating URLs used in populate_db.py

--- backend/populate_db.py
+++ backend/populate_db.py
@@ -7,9 +7,8 @@ DB_PASSWORD = "cos333"
 DB_HOST = "localhost"
 DB_PORT = "5432"
 
-OBJECTS_URL = "https://data.artmuseum.princeton.edu/objects"
-MAKERS_URL = "https://data.artmuseum.princeton.edu/makers"
-
+OBJECTS_URL = "https://static.artmuseum.princeton.edu/objects"
+MAKERS_URL = "https://static.artmuseum.princeton.edu/makers"
 
 def main():
     conn = psycopg2.connect(
