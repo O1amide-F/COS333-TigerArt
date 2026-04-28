ec64fef13ccaec670503fbccb9f45f2d74e4e0de
Olamide Falayi <of5697@princeton.edu>
2026-04-28 00:34:45 -0400

    Re-updating how OFTEN News is refreshed (may be subject to change)

--- backend/server.py
+++ backend/server.py
@@ -492,6 +492,7 @@ def get_for_you():
 
 @app.route("/api/news")
 def get_news():
+    refresh_news_data()
     conn = get_connection()
     cur = conn.cursor()
 
@@ -1314,5 +1315,4 @@ def get_user_preferences(user_id):
 
 
 if __name__ == "__main__":
-    refresh_news_data()
     app.run(debug=True, port=5001, host='0.0.0.0')
