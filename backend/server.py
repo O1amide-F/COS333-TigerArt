564851ca83ef4ad78da5937d22a48fe95674c426
Olamide Falayi <of5697@princeton.edu>
2026-04-28 01:05:06 -0400

    reverting previous change

--- backend/server.py
+++ backend/server.py
@@ -492,7 +492,6 @@ def get_for_you():
 
 @app.route("/api/news")
 def get_news():
-    refresh_news_data()
     conn = get_connection()
     cur = conn.cursor()
 
@@ -1315,4 +1314,5 @@ def get_user_preferences(user_id):
 
 
 if __name__ == "__main__":
+    refresh_news_data()
     app.run(debug=True, port=5001, host='0.0.0.0')
